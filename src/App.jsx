2e0e0cb13a01c9a51f16622d76ce90e18840c602
daniel <muriithidaniel977@gmail.com>
2025-03-11 18:16:26 +0300

    added buyers

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import Dashboard from "./components/Pages/Dashboard";
 import Analytics from "./components/Pages/Analytics";
 import NotFound from "./components/NotFound"; // Add this component
 import Farmers from "./components/Pages/Farmers";
-import NewSale from "./components/Pages/NewSale";
+import NewSale from "./components/Pages/NewOrder";
+import Buyers from "./components/Pages/Buyers";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
   <Route path="NewSale" element={<NewSale />} />
   <Route path="analytics" element={<Analytics />} />
   <Route path="farmers" element={<Farmers />} />
+  <Route path="Buyers" element={<Buyers />} />
 </Route>
 
         <Route path="*" element={<NotFound />} />
